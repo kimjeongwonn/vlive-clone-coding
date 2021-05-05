c2fb20aecebb69227e68ff6b6871fd30e78fc6d3
dooyeong20 <noviced22@gmail.com>
2021-05-05 12:02:34 +0900

    feat: Video and Video Header Sticky
    with javascript Done

--- 01_team/src/js/dy.js
+++ 01_team/src/js/dy.js
@@ -1,1 +1,14 @@
-console.log("hello dy");
+const header = document.getElementsByClassName("video__header")[0];
+const video = document.getElementsByClassName("video__content")[0];
+
+document.addEventListener("scroll", (e) => {
+  const bodyRect = document.body.getBoundingClientRect();
+
+  if (bodyRect.y <= -60) {
+    header.classList.add("is-fixed");
+    video.classList.add("is-fixed");
+  } else {
+    header.classList.remove("is-fixed");
+    video.classList.remove("is-fixed");
+  }
+});
